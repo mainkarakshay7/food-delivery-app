be1890afc13fc7f5ebda8ab37efacd68682650e7
Akshay Mainkar <mainkarakshay7@gmail.com>
2024-07-10 19:36:52 +0530

    test module - incomplete commit

--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -16,7 +16,10 @@ const useRestaurant = (id) => {
 
     const menuList =
       data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
-        ?.card?.itemCards ?? [];
+        ?.card?.itemCards ??
+      data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card
+        ?.card?.itemCards ??
+      [];
 
     const menu = new Array();
 
